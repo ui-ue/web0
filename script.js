9f1ca1348e9905277bdab18b57887a4b4d0b4fc2
ui-ue <u1nuu2@gmail.com>
2026-05-31 23:42:22 +1000

    feat: add basic click event listener to sections

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //hi
 const sections = document.querySelectorAll('.time-block');
 console.log("Found sections:", sections);
+const sections = document.querySelectorAll('.time-block');
+
+sections.forEach((section) => {
+    section.addEventListener('click', () => {
+        console.log("Section clicked!");
+    });
+});
