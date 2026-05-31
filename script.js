ea7e0cef594d3b7704593cb68c1e6230f32fce92
ui-ue <u1nuu2@gmail.com>
2026-05-31 23:44:03 +1000

    item creation

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ sections.forEach((section) => {
         console.log("Section clicked!");
     });
 });
+function createItem(section) {
+    const item = document.createElement('div');
+    item.innerText = '🍃';
+    item.style.position = 'absolute';
+    item.style.top = '10px';
+    item.style.left = '50%'; 
+    section.appendChild(item);
+}
+
+const sections = document.querySelectorAll('.time-block');
+sections.forEach((section) => {
+    section.addEventListener('click', () => {
+        createItem(section);
+    });
+});
